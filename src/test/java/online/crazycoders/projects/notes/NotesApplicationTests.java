63bc79fa993244596fe04ea39a3c8c6a7ea860a5
programmercommunity-manimaran <programmercommunity.manimaran@gmail.com>
2021-05-29 19:16:35 +0530

    initial commit

---
+++ src/test/java/online/crazycoders/projects/notes/NotesApplicationTests.java
@@ -0,0 +1,13 @@
+package online.crazycoders.projects.notes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
