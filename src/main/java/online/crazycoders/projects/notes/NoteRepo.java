40e0bb02cb1e261736fa3cd08d983dfdc44709a3
Manimaran-Bhuwaneshwaran <62472418+Manimaran-Bhuwaneshwaran@users.noreply.github.com>
2021-05-29 22:10:11 +0530

    Delete NoteRepo.java

--- src/main/java/online/crazycoders/projects/notes/NoteRepo.java
+++
@@ -1,7 +0,0 @@
-package online.crazycoders.projects.notes;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface NoteRepo extends MongoRepository<Note, String> {
-
-}
